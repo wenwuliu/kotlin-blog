0a16ab299818e11e5f537b6681b966f7e08193d6
liuwenwu <liuawu625@163.com>
2019-09-18 23:27:44 +0800

    提交

---
+++ life-program-institute/src/main/java/com/belphegor/lifeprograminstitute/service/UserService.java
@@ -0,0 +1,4 @@
+package com.belphegor.lifeprograminstitute.service;
+
+public interface UserService {
+}
---
+++ life-program-institute/src/main/java/com/belphegor/lifeprograminstitute/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.belphegor.lifeprograminstitute.service.impl;
+
+import com.belphegor.lifeprograminstitute.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
